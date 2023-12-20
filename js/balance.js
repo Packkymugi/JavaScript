70375ce116d52394cf997832dc9bba160c338513
Wichapon V <wichapon@gmail.com>
2023-12-20 23:10:40 +0700

    add comment

--- js/balance.js
+++ js/balance.js
@@ -3,37 +3,58 @@ let list = document.getElementById("expense-list");
 let total = document.getElementById("total-amount");
 let exName = document.getElementById("expense-name");
 let exAmount = document.getElementById("expense-amount");
+
 let expenses = [];
 
 function addExpense(event) {
     event.preventDefault();
-    let name = exName.value;
-    let amount = exAmount.value;
+    let inputName = exName.value;
+    let inputAmount = exAmount.value;
     
     exName.value = ""; 
     exAmount.value = ""; 
 
-    let e = {
-        name: name,
-        amount: parseFloat(amount),
+    let expense = {
+        name: inputName ,// input name
+        amount: parseFloat(inputAmount), // 
     }
-    expenses.push(e);
+    expenses.push(expense);
+
+    console.log(expenses);
+
     renderExpenses();
 }
+
 function renderExpenses(){
         let totalAmount = 0;
         let i = 0 ;
-        list.innerHTML = "";
+        let list = document.getElementById("expense-list");
+        // list.innerHTML = "<h1>PAAAAAAACCCCCCKYYY</h1>";
+        list.innerHTML="";
 
         for (let exp of expenses) {
             totalAmount = totalAmount + exp.amount;
-            let expenseRow = document.createElement("tr");
+            let expenseRow = document.createElement("tr"); // create tr(row) for input data
      
-            expenseRow.innerHTML = `
+            expenseRow.innerHTML = 
+
+            // exp = {
+            //     amount: 123,
+            //     name: "tttttt"
+            // }
+
+            /*
+            "<td>"+ exp.name +"</td>"
+            "<td>   "    + exp.amount +"</td>"
+            */
+
+            `
             <td>${exp.name}</td>
             <td>${exp.amount}</td>
             <td class= "delete-btn" data-id = "${i++}">Delete</td>
             `
+            
+
             list.appendChild(expenseRow);
         }
         total.textContent = totalAmount.toFixed(2);
@@ -52,5 +73,6 @@ function deleteExpense(event){
         renderExpenses();
     }
 }
+
 form.onsubmit = addExpense;
 list.onclick = deleteExpense;
